9b3e3a6c32728d1c424e4daf72e6de66ded857ed
Aspitrine <loquente62@gmail.com>
2023-11-15 09:35:35 +0100

    Fix erreur qui fait planter tous le site

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,15 +1,14 @@
-import { useSelector } from 'react-redux';
-
+import { useAppSelector } from '../../hooks/redux';
 import './styles.scss';
 
 function Menu() {
-  const recipes = useSelector((state: any) => state.recipes);
+  const recipes = useAppSelector((state) => state.recipes.list);
   return (
     <nav className="menu">
       <a className="menu-link menu-link--active" href="/">
         Accueil
       </a>
-      {recipes.map((recipe: any) => (
+      {recipes.map((recipe) => (
         <a
           key={recipe.id}
           className="menu-link"
