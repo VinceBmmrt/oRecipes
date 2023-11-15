1ea01a3861dd6b60440919dd7d90dda78c5ca73b
Aspitrine <loquente62@gmail.com>
2023-11-15 09:56:02 +0100

    Réparation de la difficulté

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,17 +3,17 @@ import './styles.scss';
 type CardProps = {
   thumbnail: string;
   title: string;
-  level: string;
+  difficulty: string;
   slug: string;
 };
 
-function Card({ thumbnail, title, level, slug }: CardProps) {
+function Card({ thumbnail, title, difficulty, slug }: CardProps) {
   return (
     <article className="card">
       <img className="card-img" src={thumbnail} alt={title} />
       <div className="card-content">
         <h2 className="card-title">{title}</h2>
-        <p className="card-desc">Difficulté :{level}</p>
+        <p className="card-desc">Difficulté : {difficulty}</p>
         <a href={`/recipe/${slug}`} className="card-link">
           Voir la recette
         </a>
