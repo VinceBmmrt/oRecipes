1ee36e76f3025a011e9fac207656f201623f94cb
Aspitrine <loquente62@gmail.com>
2023-11-15 14:52:16 +0100

    Ajout de l'appel API pour récupérer les recettes

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,16 +1,26 @@
+import { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Menu from '../Menu';
 
 import Loading from './Loading';
 
 import './App.scss';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { fetchRecipes } from '../../store/reducers/recipes';
 
-type AppProps = {
-  loading?: boolean;
-};
+function App() {
+  // Pour emettre une action / intention j'ai besoin de récupérer dispatch
+  const dispatch = useAppDispatch();
 
-function App({ loading }: AppProps) {
-  if (loading) {
+  // Lorsque mon composant se monte la 1ère fois, j'emet l'intention de récupérer les recettes
+  useEffect(() => {
+    // De manière général, lorsqu'on appel une API au chargement de la page, il y aura TOUJOURS un useEffect quelque part
+    dispatch(fetchRecipes());
+  }, [dispatch]);
+
+  const isLoading = useAppSelector((state) => state.recipes.isLoading);
+
+  if (isLoading) {
     return <Loading />;
   }
   return (
