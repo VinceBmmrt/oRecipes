6ff49e9df5c014a1d75962d9512327fcc8f8f74a
Aspitrine <loquente62@gmail.com>
2023-11-17 09:35:30 +0100

    Ajout des données de credentials dans le store + récupération côté AppHeader

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,14 +1,19 @@
 import './styles.scss';
 import logo from '../../assets/logo.png';
 import LoginForm from '../LoginForm';
+import { useAppSelector } from '../../hooks/redux';
 
 function AppHeader() {
+  const emailValue = useAppSelector((state) => state.user.credentials.email);
+  const passwordValue = useAppSelector(
+    (state) => state.user.credentials.password
+  );
   return (
     <header className="header">
       <img src={logo} className="header-logo" alt="Logo oRecipes" />
       <LoginForm
-        email="toto"
-        password="tata"
+        email={emailValue}
+        password={passwordValue}
         changeField={() => {}}
         handleLogin={() => {}}
         handleLogout={() => {}}
--- src/store/reducers/user.ts
+++ src/store/reducers/user.ts
@@ -2,9 +2,17 @@ import { createSlice } from '@reduxjs/toolkit';
 
 interface UserState {
   logged: boolean;
+  credentials: {
+    email: string;
+    password: string;
+  };
 }
 export const initialState: UserState = {
   logged: false,
+  credentials: {
+    email: 'toto@toto.fr',
+    password: 'toto',
+  },
 };
 
 const userReducer = createSlice({
