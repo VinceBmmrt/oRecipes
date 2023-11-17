499b4482cd227308d62039b768698b7ad3469a0c
Aspitrine <loquente62@gmail.com>
2023-11-17 11:01:01 +0100

    Gestion de l'affichage des infos lors de la requête (loading / message d'erreur / affichage si connecter)

--- src/components/AppHeader/index.tsx
+++ src/components/AppHeader/index.tsx
@@ -1,4 +1,5 @@
 import './styles.scss';
+import clsx from 'clsx';
 import logo from '../../assets/logo.png';
 import LoginForm from '../LoginForm';
 import { useAppDispatch, useAppSelector } from '../../hooks/redux';
@@ -10,6 +11,11 @@ function AppHeader() {
   const passwordValue = useAppSelector(
     (state) => state.user.credentials.password
   );
+  const isLogged = useAppSelector((state) => state.user.logged);
+  const isLoading = useAppSelector((state) => state.user.isLoading);
+  const pseudo = useAppSelector((state) => state.user.pseudo);
+  const error = useAppSelector((state) => state.user.error);
+
   const handleChangeField = (value: string, name: 'email' | 'password') => {
     // J'emet l'intention de changer la valeur d'un champ
     // Le nom du champ est ma variable name, et la valeur est ma variable value
@@ -28,13 +34,22 @@ function AppHeader() {
   return (
     <header className="header">
       <img src={logo} className="header-logo" alt="Logo oRecipes" />
-      <LoginForm
-        email={emailValue}
-        password={passwordValue}
-        changeField={handleChangeField}
-        handleLogin={handleLogin}
-        handleLogout={() => {}}
-      />
+      <div
+        className={clsx({
+          loader: isLoading,
+        })}
+      >
+        <LoginForm
+          email={emailValue}
+          password={passwordValue}
+          changeField={handleChangeField}
+          handleLogin={handleLogin}
+          handleLogout={() => {}}
+          isLogged={isLogged}
+          loggedMessage={`Bonjour, toi aussi tu viens pour le donjon ${pseudo} ?`}
+        />
+        {error && <p className="error">{error}</p>}
+      </div>
     </header>
   );
 }
